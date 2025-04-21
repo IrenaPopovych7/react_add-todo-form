2db84e32bc67abdfc4945f18b70a8ff9f0381b20
Iryna <popovicirina40@gmail.com>
2025-04-21 10:41:28 +0100

    demo link

--- src/service/UserService.ts
+++ src/service/UserService.ts
@@ -4,16 +4,16 @@ import { User } from '../types/User';
 const initialUsers = [...usersFromServer];
 
 export function getAllUsers(): User[] {
-    return [...initialUsers];
+  return [...initialUsers];
 }
 
 export function getUserById(userId: number): User {
-    const usr: User = {
-        id: 0,
-        name: '',
-        username: '',
-        email: '',
-    };
+  const usr: User = {
+    id: 0,
+    name: '',
+    username: '',
+    email: '',
+  };
 
-    return usersFromServer.find(user => user.id === userId) || usr;
+  return usersFromServer.find(user => user.id === userId) || usr;
 }
--- src/types/Todos.ts
+++ src/types/Todos.ts
@@ -1,6 +1,6 @@
 export interface Todos {
-    id: number;
-    title: string;
-    completed: boolean;
-    userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+  userId: number;
 }
